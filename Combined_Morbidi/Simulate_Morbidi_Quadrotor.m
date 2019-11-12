3a904fb24de4f5a9c92694ade0992c9c620c1331
gmcampbell <gmcampbell@users.noreply.github.com>
2019-11-12 14:31:34 -0500

    Dynamics Work 11/12/19
    Attempted to fix dyanmics, ultimately ended up just sicking with collocation for the time being. Equations should be identical to what was done in HW5. Plotting in Simulate... was also updated.

--- Combined_Morbidi/Simulate_Morbidi_Quadrotor.m
+++ Combined_Morbidi/Simulate_Morbidi_Quadrotor.m
@@ -4,7 +4,7 @@
 
 %Function to actually Simulate Quadrotor for specified path
 
-function Simulate_Morbidi_Quadrotor
+function [X,X0,u] = Simulate_Morbidi_Quadrotor
 
 consts = constants();
 nx = 16;
@@ -12,7 +12,7 @@ nu = 4;
 
 wh = consts.wh;
 
-Final_Position = [8;10;6]; %m
+Final_Position = [4;5;6]; %m
 
 xf = Final_Position(1);
 yf = Final_Position(2);
@@ -25,25 +25,31 @@ N = 100; %Per page 5 (IV - A Scenario 1)
 tf = 20; %s (Per page 5 IV - A Scenario 1)
 dt = tf/N; %s
 
-z = find_flight_trajectory(xt0, xtf, N, dt);
+[z, Aeq, beq, lb, ub, z0] = find_flight_trajectory(xt0, xtf, N, dt);
 
 u = zeros(nu,0);
 X = zeros(nx,0);
+X0 = zeros(nx,0);
 
 for i=1:N
    [x_i_inds, u_i_inds] = sample_indices(i, nx, nu);
    
    X(:,i) = z(x_i_inds);
    u(:,i) = z(u_i_inds);
+   X0(:,i) = z0(x_i_inds);
 end
 
 %Plot x,y,z positions
 figure;
 plot3(X(1,:),X(3,:),X(5,:));
+hold on;
+grid on;
+plot3(X0(1,:),X0(3,:),X0(5,:));
 title('x,y,z position for quadrotor');
 xlabel('x');
 ylabel('y');
 zlabel('z');
+axis([-5 5 -4 4 0 12]);
 
 end
 
